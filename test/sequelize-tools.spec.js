c2e815bc65cb0307ed9e54457ce1e7de49d2d8ca
t ddddddd <miscmisc@cmme.org>
2014-02-27 15:08:52 -0800

    initialize test infrastructure. failing test for getting config file location from env var

---
+++ test/sequelize-tools.spec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+process.env.NODE_ENV            = "test";
+process.env.SEQUELIZE_DB_CONFIG = "test/config/database.js";
+
+var expect = require("expect.js"),
+    Path   = require("path"),
+    stools = require(require.resolve(Path.resolve("index"))),
+    db     = stools.db;
+
+describe("db", function() {
+
+  describe("chooseEnvConfig()", function() {
+    it("reads from the config file specified in SEQUELIZE_DB_CONFIG", function() {
+      var dbEnv = db.chooseEnvConfig();
+      expect(dbEnv.test.dbName).to.be("sequelize_tools_test");
+    });
+  });
+
+});
